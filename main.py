1f3b040506d6c42b4987b982572aa6b173c4ad03
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-08-17 11:21:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ session_list = SessionsRead()
 list_function = MenuSettings()
 
 console.print('''[bold]
+
+
+   _____      _                             _____              
+  / ____|    (_)                           |_   _|             
+ | |     _ __ _ _ __ ___  ___  ___  _ __     | |  _ __   ___   
+ | |    | '__| | '_ ` _ \/ __|/ _ \| '_ \    | | | '_ \ / __|  
+ | |____| |  | | | | | | \__ \ (_) | | | |  _| |_| | | | (__ _ 
+  \_____|_|  |_|_| |_| |_|___/\___/|_| |_| |_____|_| |_|\___(_)
+                                                               
+                                          Crimson never die. Crimson never down.                     
+
 GitHub: https://github.com/CrimsonCoalition
 Author's channel: 
 BadCord Version: v.0.1.''')
@@ -19,7 +30,7 @@ def botnet_main():
 
     menu_function={}
 
-    console.print(f'[bold white]botnet accounts >> {len(session_list.sessions)}')
+    console.print(f'[bold white]Кол-во ботнет аккаунтов: >> {len(session_list.sessions)}')
     for num_function, function in enumerate(
             list_function.menu_botnet,
             start=1
