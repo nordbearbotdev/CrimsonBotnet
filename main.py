1d7fe686e931a82f01255bd97803e4f68b15c7bf
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-08-19 18:51:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,13 @@ from functions import color
 from colorama import Fore, Style
 from termcolor import colored
 
+s.banner(text="Crimson Inc.")
 
 # print(colored( '''
 print('''
-
-
-   _____      _                             _____              
-  / ____|    (_)                           |_   _|             
- | |     _ __ _ _ __ ___  ___  ___  _ __     | |  _ __   ___   
- | |    | '__| | '_ ` _ \/ __|/ _ \| '_ \    | | | '_ \ / __|  
- | |____| |  | | | | | | \__ \ (_) | | | |  _| |_| | | | (__ _ 
-  \_____|_|  |_|_| |_| |_|___/\___/|_| |_| |_____|_| |_|\___(_)
-                                                               
-                                          Crimson never die. Crimson never down.                     
+            
+                                         
+Crimson never die. Crimson never down.                     
 
 GitHub: https://github.com/CrimsonCoalition
 Author's channel: https://t.me/CrimsonCoalition/
