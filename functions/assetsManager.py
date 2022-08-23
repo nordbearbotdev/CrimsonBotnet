7d8b8dc5c1f8b04009c917d3815421160e26d776
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-08-23 08:07:25 +0300

    Update assetsManager.py

--- functions/assetsManager.py
+++ functions/assetsManager.py
@@ -1,1 +1,19 @@
+import random
 
+def userAgent():
+    
+    with open("./assets/userAgents.txt" , encoding="utf-8") as f:
+        return random.choice(f.readlines()).split("\n")[0]
+
+def proxies():
+   
+    with open("./assets/proxies.txt" , encoding="utf-8") as f:
+        return random.choice(f.readlines()).split("\n")[0]
+
+def tokens():
+	
+	tokens = []
+	with open("./assets/tokens.txt" , encoding="utf-8") as f:
+		for i in f.readlines():
+			tokens.append(i.split("\n")[0])
+		return tokens 
